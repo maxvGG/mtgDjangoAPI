d9b0dfc7b41f6e984a9b5c0fdce076368f1f00b2
max van gorp <gorpmrh@ad.hva.nl>
2023-01-16 15:58:22 +0100

    removed react and changed to class based views

--- mtg_api/urls.py
+++ mtg_api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from . import views
 
 app_name = 'mtg_api'
 
 urlpatterns = [
-    path('', views.api_overview, name="api-overview"),
-    path('card-name/<str:pk>/', views.search_card_by_name, name="card-detail"),
-    path('random-card/', views.random_card, name='random-card'),
-    path('set/<str:pk>', views.search_cards_from_set, name='card-set')
+    path('', views.ApiOverview.as_view(), name="api-overview"),
+    path('card-name/<str:pk>/', views.CardByName.as_view(), name="card-detail"),
+    path('random-card/', views.RandomCard.as_view(), name='random-card'),
+    path('set/<str:pk>', views.SearchCardsFromSet.as_view(), name='card-set')
 ]
+
--- mtg_api/views.py
+++ mtg_api/views.py
@@ -1,56 +1,51 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 import requests
 
 
 # Create your views here.
-
-@api_view(['GET'])
-def api_overview(request):
+class ApiOverview(APIView):
     """
-    :return: an overview of all the different possible API endpoints
+    List api overview
     """
-    api_urls = {
-        'Card by name': '/card-name/<Card name>/',
-        'Random card': '/random-card/',
-        'Cards from the same set': '/set/<set abbreviation>/'
-    }
+    def get(self, request):
+        api_urls = {
+            'Card by name': '/api/card-name/<Card name>/',
+            'Random card': '/api/random-card/',
+            'Cards from the same set': '/api/set/<set abbreviation>/'
+        }
+        return Response(api_urls)
 
-    return Response(api_urls)
 
-
-@api_view(['GET'])
-def search_card_by_name(request, pk):
+class CardByName(APIView):
     """
     :param pk: card name
     :return: card info in JSON format
     """
-    # searches for a card
-    card = requests.get('https://api.scryfall.com/cards/named?fuzzy=' + pk)
-    data = card.json()
-    return Response(data)
+    def get(self, request, pk):
+        card = requests.get('https://api.scryfall.com/cards/named?fuzzy=' + pk)
+        data = card.json()
+        return Response({
+            "name": data['name'],
+            "url": data['scryfall_uri'],
+            "image": data['image_uris']["normal"],
+        })
 
 
-@api_view(['GET'])
-def random_card(request):
-    """
-    :return: a random card from Scryfall in json format
-    """
-    card = requests.get('https://api.scryfall.com/cards/random')
-    data = card.json()
-    return Response({
-        "name": data['name'],
-        "url": data['scryfall_uri'],
-        "image": data['image_uris']["normal"],
-    })
+class RandomCard(APIView):
+    def get(self, request):
+        card = requests.get('https://api.scryfall.com/cards/random')
+        data = card.json()
+        return Response({
+            "name": data['name'],
+            "url": data['scryfall_uri'],
+            "image": data['image_uris']["normal"],
+        })
 
 
-@api_view(['GET'])
-def search_cards_from_set(request, pk):
-    """
-    :param pk: set name
-    :return: all the cards from the same set in JSON format
-    """
-    card = requests.get('https://api.scryfall.com/cards/search?q=(set:' + pk + ')')
-    data = card.json()
-    return Response(data)
+class SearchCardsFromSet(APIView):
+    def get(self, request, pk):
+        card = requests.get('https://api.scryfall.com/cards/search?q=(set:' + pk + ')')
+        data = card.json()
+        return Response(data)
+
